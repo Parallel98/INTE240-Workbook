7f2415b5c2afa26c7c8c8532f7cca1d1b5d91edc
Parallel 98 <tylerobrown03@gmail.com>
2025-03-18 03:26:38 -0400

    create Another One Bites the Dust

---
+++ HTML-Labs/JavaScript_Labs/JavaScript Lab - Another One Bites the Dust/duetscript.js
@@ -0,0 +1,284 @@
+const lyricsFreddie = [
+    { time: 0, text: "Are you ready, hey, are you ready for this?" },
+    { time: 2.2, text: "Are you hanging on the edge of your seat?" },
+    { time: 4.5, text: "Are you ready, hey, are you ready for this?"},
+    { time: 6.8, text: "Are you hanging on the edge of your seat?" },
+    { time: 9, text: "Are you ready, hey, are you ready for this?" },
+    { time: 11.3, text: "Are you hanging on the edge of your seat?" },
+    { time: 14, text: "Outta the doorway the bullets rip" },
+    { time: 16.3, text: "Repeating to the sound of the beat, hey" },
+    { time: 18.5, text: ". . . ." },
+    
+    { time: 20.8, text: "Another one bites the dust" },
+    { time: 22.8, text: ". . . ." },
+
+    { time: 25, text: "Another one bites the dust" },
+    { time: 27, text: ". . . ." },
+
+    { time: 30, text: "Another one bites the dust" },
+    { time: 32, text: "And another one gone" },
+    { time: 33.5, text: "And another one gone" },
+    { time: 34, text: "Another one bites the dust" },
+    { time: 36.5, text: "Steve walks wearily down the street" },
+    { time: 39, text: "With the brim pulled way down low" },
+    { time: 41, text: ". . . ." },
+
+    { time: 45.5, text: "Are you ready, hey, are you ready for this?" },
+    { time: 47.5, text: "Are you hanging on the edge of your seat?" },
+    { time: 50, text: "Outta the doorway the bullets rip" },
+    { time: 52.3, text: "Repeating to the sound of the beat, hey" },
+
+    { time: 56.8, text: "Another one bites the dust" },
+    { time: 58.8, text: ". . . ." },
+
+    { time: 61.5, text: "Another one bites the dust" },
+    { time: 63.5, text: "And another one gone" },
+    { time: 65, text: "And another one gone" },
+    { time: 66, text: "Another one bites the dust" },
+
+    { time: 72, text: "Oh yeah" },
+    { time: 73, text: ". . . ." },
+
+    { time: 86, text: "Come on" },
+    { time: 87, text: ". . . ." },
+
+    { time: 94.8, text: "Oh yeah" },
+    { time: 95.8, text: ". . . ." },
+
+    { time: 99.5, text: "Oh yeah" },
+    { time: 100.5, text: ". . . ." },
+
+    { time: 103.5, text: "Oh yeah" },
+    { time: 104.5, text: ". . . ." },
+
+    { time: 108.5, text: "Oh yeah" },
+    { time: 109.5, text: ". . . ." },
+
+    { time: 111.5, text: "Another one bites the dust" },
+    { time: 113.5, text: ". . . ." },
+
+    { time: 115.5, text: "Another one bites the dust" },
+    { time: 117.5, text: "Aw yeah" },
+
+    { time: 118.5, text: "And another one gone" },
+    { time: 119.5, text: "And another one gone" },
+    { time: 120.5, text: "Another one bites the dust" },
+    { time: 122.5, text: "How do you think I'm gonna get along" },
+    { time: 125, text: "Without you when you're gone" },
+    { time: 126.5, text: "Oh yeah" },
+    { time: 127.5, text: ". . . ." },
+
+    { time: 129.5, text: "Kicked me out on my own" },
+    { time: 131.5, text: "Are you happy, are you satisfied?" },
+    { time: 134, text: "How long can you stand the heat?" },
+    { time: 136.5, text: "Outta the doorway, the bullets rip" },
+    { time: 139, text: "To the sound of the beat, look out" },
+
+    { time: 143, text: "Another one bites the dust" },
+    { time: 145, text: ". . . ." },
+    { time: 147, text: "Another one bites the dust" },
+    { time: 149, text: ". . . ." },
+    { time: 152, text: "Another one bites the dust" },
+    { time: 154, text: "And another one gone" },
+    { time: 156, text: "And another one gone" },
+    { time: 156.5, text: "Another one bites the dust" },
+    { time: 159, text: ". . . ." },
+
+    { time: 163, text: "Oh yeah" },
+    { time: 165, text: ". . . ." },
+
+    { time: 167.5, text: "Oh yeah" },
+    { time: 169.5, text: ". . . ." },
+
+    { time: 171.5, text: "Oh yeah" },
+    { time: 173.5, text: ". . . ." },
+
+    { time: 175.5, text: "Oh yeah" },
+    { time: 177.5, text: ". . . ." },
+
+    { time: 180.5, text: "Oh yeah" },
+    { time: 182.5, text: ". . . ." },
+
+    { time: 185, text: "Oh yeah/Navy SEAL!" },
+    { time: 187, text: ". . . ." },
+
+    { time: 189.5, text: "Oh yeah" },
+    { time: 191.5, text: ". . . ." },
+
+    { time: 194.5, text: "Oh yeah" },
+    { time: 196.5, text: ". . . ." },
+
+    { time: 198.5, text: "Oh yeah" },
+    { time: 200.5, text: ". . . ." },
+
+    { time: 202.5, text: "Oh yeah" },
+    { time: 204.5, text: ". . . ." },
+
+    { time: 208, text: "Oh yeah" },
+    { time: 210, text: ". . . ." },
+
+    { time: 212.5, text: "Oh yeah" },
+    { time: 214.5, text: ". . . ." },
+
+    { time: 215.5, text: "Another one bites the dust, Oh yeah" },
+    { time: 217.5, text: ". . . ." },
+    { time: 220, text: "Another one bites the dust" },
+    { time: 222, text: ". . . ." },
+    { time: 224, text: "Another one bites the dust" },
+    { time: 226, text: ". . . ." },
+    { time: 229, text: "Another one bites the dust" },
+    { time: 231, text: ". . . ." },
+    { time: 231.5, text: "Don't you know they're coming for you" },
+    { time: 233, text: "Dont' you know they're coming for you (Another one bites the dust)" },
+    { time: 236, text: ". . . ." },
+
+    { time: 238.5, text: "Another one bites the dust" },
+    { time: 240, text: "And another one gone" },
+    { time: 242, text: "And another one gone" },
+    { time: 242.5, text: "Another one bites the dust, yeah" },
+    { time: 246.5, text: ". . . ." },
+
+    { time: 247.5, text: "Another one bites the dust" },
+    { time: 249.5, text: ". . . ." },
+
+    { time: 251.5, text: "Another one bites the dust" },
+    { time: 253.5, text: ". . . ." },
+
+    { time: 255.5, text: "Another one bites the dust" },
+    { time: 257.5, text: ". . . ." },
+
+    
+   ];
+
+const lyricsOthers = [
+    { time: 3.8, text: "I need a break beat, uh" },
+    { time: 8.8, text: "I need a break beat, uh" },
+    { time: 13.8, text: "I need a break beat, uh" },
+    { time: 18, text: "Yo, a for the kids in the club that's ready to get bugged" },
+    { time: 22.5, text: "And for the thugs with the burners that wanna blast off " },
+    { time: 27, text: "And for the kids on the blocks, shootin' at the crooked cops—blaow! " },
+    { time: 32.5, text: "Wyclef" },
+    { time: 33.5, text: "Dirty Cash" },
+    { time: 34.4, text: "Young Free, Freddie where you at?" },
+    { time: 38, text: ". . . ." },
+
+    { time: 41, text: "Some cat up in Brooklyn just got robbed with a Kangol " },
+    { time: 54.5, text: "My man got shot, and the block got hot " },
+    { time: 58.8, text: "Yo, hey I hear more shots, this is like Fort Knox, kid " },
+    { time: 63, text: "Yo, hold your breath" },
+    { time: 64, text: "Hold your breath" },
+    { time: 65.5, text: "Hold your breath" },
+    { time: 66.8, text: "Yo, check it" },
+
+    { time: 68, text: "If you're a soldier at ease" },
+    { time: 69.5, text: "My military style is known to murder Nazis" },
+    { time: 71.5, text: "Brooklyn to Germany, come on" },
+    { time: 73, text: "My kamikaze will blow the U2" },
+    { time: 75, text: "They hire Idi Amin in Timbuktu " },
+    { time: 77.5, text: "Whether you Hindu, or do the Voodoo " },
+    { time: 79.5, text: "You can't foresee this unless I bring the previews, yeah, yeah" },
+    { time: 84, text: "Yo, it's the number one rappin' band" },
+    { time: 86, text: "Yo, this review will be critically acclaimed" },
+    { time: 88.8, text: "Leave you in critical pain, clinically insane" },
+    { time: 91, text: "The name Wyclef Jean, with a yes, yes y'all" },
+    { time: 93.5, text: "Better have a vest y'all, I'll blast, and bless y'all" },
+    { time: 95.5, text: "F-y'all, the mark of the beast, the triple six" },
+    { time: 98, text: "Time running out, listen to the tick" },
+    { time: 100, text: "If you see what I saw, then you seen what I seen" },
+    { time: 102, text: "If you know what I know, you know what I mean" },
+    { time: 104.5, text: "Commanding officer of the Navy SEAL team" },
+    { time: 107, text: "Once I give the orders, you feel the infrared beam Blaow!" },
+    { time: 109.5, text: "For all you critics, sayin' 'another remake'" },
+
+    { time: 112.8, text: "Yo, if you know the deal, this is the master reel, kid" },
+
+    { time: 119, text: "Right" },
+    { time: 120, text: "Right" },
+    { time: 121, text: "Freddie Mercury, where you at yo?" },
+
+    { time: 126, text: " I need a break yo " },
+    { time: 127.5, text: " If you're ready for the first of the month " },
+    { time: 129, text: "For that welfare check, come on" },
+    { time: 131, text: "I need a break beat" },
+    { time: 135.5, text: "I need a break beat" },
+    { time: 140, text: "Yo, bulletproof vests, like the wild, wild west" },
+    { time: 144, text: "Yo, this is a stickup, now take off your Rolex" },
+    { time: 149, text: "Yo, dirty money, good money, yo it's all money-money" },
+    { time: 153, text: "Yo, Dirty Cash " },
+    { time: 155, text: "Dirty Cash" },
+    { time: 156, text: "The adventures of Dirty Cash" },
+
+    { time: 159, text: "Yo, for the love for the cash, I'll blast you in my path" },
+    { time: 161, text: "Keep my eyes on the math, you cats don't know the half" },
+    { time: 163.5, text: "As far as I'm concerned, you cats can burn in flames" },
+    { time: 165.8, text: "This ain't no game, I'mma start callin' names" },
+    { time: 167.5, text: "So come get me, if you know the one-fifty" },
+    { time: 170.5, text: "A million refugees ready to bust with me" },
+    { time: 172.8, text: "Bloody, filthy, in this rap shhh—" },
+    { time: 174, text: "You gonna have to kill me, since you can't beat me" },
+    { time: 177, text: "Pras, Dirty, Cash, you're the greedy" },
+    { time: 179, text: "Believe me, you gotta let me fly like R. Kelly" },
+    { time: 181.5, text: "Bite another dust with my man Freddie Mercury" },
+    { time: 184, text: "What height nineties got ya cash, wannabe crazy" },
+
+    { time: 186, text: "Practically, I tactically destroy" },
+    { time: 188, text: "Deploy more decoys than a presidential convoy" },
+    { time: 190.5, text: "My whole envoy stay camouflaged out" },
+    { time: 193, text: "And when I walk the street, I take the refugee route" },
+    { time: 195.5, text: "This one go out to all my thugs in the borough" },
+    { time: 197.5, text: "Soldiers stay thorough, like Kilamanjaro" },
+    { time: 199.5, text: "Split it with an arrow, my girl platoon roll" },
+    { time: 202, text: "Outta control, the female Mandingos" },
+    { time: 203.8, text: "Free, I evolve from the egg of a seminarian" },
+    { time: 207, text: "Don't go down, 'cause I'm a vegetarian" },
+    { time: 209, text: "And when I bust, it ain't 'in God we trust'" },
+    { time: 211, text: "And if you bring a gun, you better bring a black tusk" },
+
+    { time: 213.5, text: "She looked into my eyes and said FBI" },
+    { time: 217.5, text: "She said she loved me, she was a spy who lied" },
+    { time: 221.8, text: "Right" },
+    { time: 223, text: "I could relate, could you relate?" },
+    { time: 227, text: "Jerry 'Wonda', The Product " },
+    { time: 231, text: "Yo, Canibus, John Forté" },
+    { time: 235, text: "Yo, Dirty Cash, and baby Free" },
+    { time: 240.5, text: "Wyclef Jean, Freddie Mercury, ha-ha" },
+    { time: 245, text: "I'm out baby" },
+    { time: 247, text: "Navy SEALs!" },
+    
+   ];
+
+   let currentLyricIndexFreddie = 0;
+   let currentLyricIndexOthers = 0;
+
+   const audio = document.getElementById("audio");
+   const lyricElementFreddie = document.getElementById("lyric-freddie");
+   const lyricElementOthers = document.getElementById("lyric-others");
+
+   audio.addEventListener("play", () => {
+    setInterval(displayLyricsFreddie, 100);
+    setInterval(displayLyricsOthers, 100);
+    });
+
+   function displayLyricsFreddie() {
+    const currentTime = audio.currentTime;
+
+    if (
+        currentLyricIndexFreddie < lyricsFreddie.length &&
+        currentTime >= lyricsFreddie[currentLyricIndexFreddie].time
+    ) {
+        lyricElementFreddie.textContent = lyricsFreddie[currentLyricIndexFreddie].text;
+        currentLyricIndexFreddie++;
+    }
+   } 
+
+   function displayLyricsOthers() {
+    const currentTime = audio.currentTime;
+   if (
+    currentLyricIndexOthers < lyricsOthers.length &&
+    currentTime >= lyricsOthers[currentLyricIndexOthers].time
+) 
+{
+    lyricElementOthers.textContent = lyricsOthers[currentLyricIndexOthers].text;
+    currentLyricIndexOthers++;
+}
+   }
